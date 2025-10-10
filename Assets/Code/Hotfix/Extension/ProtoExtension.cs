a5308887b5ef25a7a6ab8a6a2572a641e2d3b68b
白茶 <FindWindPeople@163.com>
2025-10-10 18:28:08 +0800

    添加proto扩展文件

--- Assets/Code/Hotfix/Extension/ProtoExtension.cs
+++ Assets/Code/Hotfix/Extension/ProtoExtension.cs
@@ -1,3 +1,7 @@
+using Google.Protobuf;
+using System;
+using System.ComponentModel;
+using System.IO;
 namespace CamelliaGame.HotfixCode
 {
     /// <summary>
@@ -5,6 +9,58 @@ namespace CamelliaGame.HotfixCode
     /// </summary>
     public static class ProtoExtension
     {
+        public static byte[] ToBytes(object message)
+        {
+            return ( (IMessage)message ).ToByteArray( );
+        }
 
+        public static void ToStream(object message , MemoryStream stream)
+        {
+            ( (IMessage)message ).WriteTo(stream);
+        }
+
+        public static object FromBytes(Type type , byte[] bytes , int index , int count)
+        {
+            object message = Activator.CreateInstance(type);
+            ( (IMessage)message ).MergeFrom(bytes , index , count);
+            if(message is not ISupportInitialize iSupportInitialize)
+            {
+                return message;
+            }
+            iSupportInitialize.EndInit( );
+            return message;
+        }
+        public static object FromBytes(object message , byte[] bytes , int index , int count)
+        {
+            ( (IMessage)message ).MergeFrom(bytes , index , count);
+            if(message is not ISupportInitialize iSupportInitialize)
+            {
+                return message;
+            }
+            iSupportInitialize.EndInit( );
+            return message;
+        }
+
+        public static object FromStream(Type type , MemoryStream stream)
+        {
+            object message = Activator.CreateInstance(type);
+            ( (IMessage)message ).MergeFrom(stream.GetBuffer( ) , (int)stream.Position , (int)stream.Length);
+            if(message is not ISupportInitialize iSupportInitialize)
+            {
+                return message;
+            }
+            iSupportInitialize.EndInit( );
+            return message;
+        }
+        public static object FromStream(object message , MemoryStream stream)
+        {
+            ( (IMessage)message ).MergeFrom(stream.GetBuffer( ) , (int)stream.Position , (int)stream.Length);
+            if(message is not ISupportInitialize iSupportInitialize)
+            {
+                return message;
+            }
+            iSupportInitialize.EndInit( );
+            return message;
+        }
     }
 }
